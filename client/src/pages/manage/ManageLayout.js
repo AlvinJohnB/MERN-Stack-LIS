18a485fbb0309309246b8e88e6ad07cbe9923a9e
alvinbregana <alvinbregana@gmail.com>
2025-04-23 06:25:32 +0800

    Changed Layout navbars to react-bootstrap nav

--- client/src/pages/manage/ManageLayout.js
+++ client/src/pages/manage/ManageLayout.js
@@ -5,14 +5,18 @@ import { jwtDecode } from 'jwt-decode';
 import { AuthContext } from './../../contexts/AuthContext'; // Import AuthContext
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 
-export default function ManageLayout() {
+export default function LabLayout() {
 
   const [user, setUser] = useState({})
   
   const { logout } = useContext(AuthContext); // Access login function from AuthContext
-  
+  const [expanded, setExpanded] = useState(false); // State to manage navbar collapse
+
   useEffect(() => {
   
       const token = Cookies.get('session'); // Retrieve the session cookie
@@ -22,83 +26,69 @@ export default function ManageLayout() {
       }
     }, [])
 
+    const handleNavLinkClick = () => {
+      setExpanded(false); // Collapse the navbar when a link is clicked
+    };
+
   return (
-        <div 
-          className="d-flex justify-content-center align-items-center"
-          style={{ 
-              height: '100vh',
-              backgroundImage: 'url(/images/background.jpg)',
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-            }}>
           <div
-            className="container m-0 p-0 shadow"
+          className="d-flex justify-content-center"
+          style={{
+            height: '100vh',
+            backgroundImage: 'url(/images/background.jpg)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'repeat',
+          }}
+        >
+          <div
+            className="container-fluid mx-0 my-auto p-0 shadow"
             style={{
               maxWidth: '80%',
-              height: 'auto',
-              minHeight: '80vh',
+              height: '90vh',
               borderRadius: '5px',
-              backgroundColor: 'rgba(255, 255, 255, 0.76)',
+              backgroundColor: 'rgba(207, 207, 207, 0.9)',
+              overflowY: 'auto',
             }}
           >
             {/* Bootstrap Navbar */}
             
-            <nav className="navbar navbar-expand-lg bg-primary m-0">
-              <div className="container-fluid">
-                <Link className="navbar-brand text-white" to="/">
-                  MyApp
-                </Link>
-                <button
-                  className="navbar-toggler"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#navbarNav"
-                  aria-controls="navbarNav"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                  <ul className="navbar-nav me-auto">
-
-                  <li className="nav-item">
-                      <Link className="nav-link text-white" to="/">
-                        Reception
-                      </Link>
-                    </li>
-
-                    <li className="nav-item">
-                      <Link className="nav-link text-white" to="/lab/Chemistry">
-                        Laboratory
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link text-white" to="/lab/Hematology">
-                        Tests
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link className="nav-link text-white" to="/lab/Clinical Microscopy">
-                        Packages
-                      </Link>
-                    </li>
-                  </ul>
+            <Navbar className="bg-primary m-0" collapseOnSelect expand="xl" expanded={expanded}>
+              <Container>
+                <Navbar.Brand className='text-white' href="#home">MyApp</Navbar.Brand>
+                <Navbar.Toggle onClick={() => setExpanded(!expanded)} aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                  <Nav className="me-auto">
+                    <Nav.Link onClick={handleNavLinkClick} className='text-white' as={Link} to="/">Reception</Nav.Link>
+                    <Nav.Link onClick={handleNavLinkClick} className='text-white' as={Link} to="/lab/Chemistry">Laboratory</Nav.Link>
+                    <Nav.Link onClick={handleNavLinkClick} className='text-white' as={Link} to="/manage">Test</Nav.Link>
+                    <Nav.Link onClick={handleNavLinkClick} className='text-white' as={Link} to="/lab/Clinical Microscopy">Packages</Nav.Link>
+                    {/* <Nav.Link onClick={handleNavLinkClick} className='text-white' as={Link} to="/lab/Serology">Serology</Nav.Link> */}
+                   
+                  </Nav>
+                  <Navbar.Text>
                   {user && (
                     <DropdownButton
-                      size='sm'
+                      size="sm"
                       id="dropdown-basic-button"
-                      variant='primary'
-                      title={`${user.name}`} // Display username from token
+                      variant="primary"
+                      title={` ${user.name}`} // Display username from token
                     >
-                      <Dropdown.Item onClick={logout}>Log out</Dropdown.Item>
+                      <Dropdown.Item
+                        onClick={() => {
+                          logout();
+                          handleNavLinkClick(); // Collapse navbar on logout
+                        }}
+                      >
+                        Log out
+                      </Dropdown.Item>
                     </DropdownButton>
-                    
                   )}
-                </div>
-              </div>
-            </nav>
+                </Navbar.Text>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
+
 
             {/* Main Content */}
             <div className="mt-4">
--- client/src/pages/reception/Layout.js
+++ client/src/pages/reception/Layout.js
@@ -5,12 +5,17 @@ import { jwtDecode } from 'jwt-decode';
 import { AuthContext } from './../../contexts/AuthContext'; // Import AuthContext
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 export default function Layout() {
 
   const [user, setUser] = useState({})
 
   const { logout } = useContext(AuthContext); // Access login function from AuthContext
+  const [expanded, setExpanded] = useState(false); // State to manage navbar collapse
+
 
   useEffect(() => {
 
@@ -21,91 +26,122 @@ export default function Layout() {
     }
   }, [])
 
+  const handleNavLinkClick = () => {
+    setExpanded(false); // Collapse the navbar when a link is clicked
+  };
+
 
   return (
     <div
-    className="d-flex justify-content-center align-items-center"
+    className="d-flex justify-content-center"
     style={{
       height: '100vh',
       backgroundImage: 'url(/images/background.jpg)',
       backgroundSize: 'cover',
       backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat',
+      backgroundRepeat: 'repeat',
     }}
   >
     <div
-      className="container-fluid m-0 p-0 shadow"
+      className="container-fluid mx-0 my-auto p-0 shadow"
       style={{
         maxWidth: '80%',
-        height: 'auto',
-        minHeight: '80vh',
+        height: '90vh',
         borderRadius: '5px',
         backgroundColor: 'rgba(207, 207, 207, 0.9)',
+        overflowY: 'auto',
       }}
     >
-      <nav className="navbar navbar-expand-lg bg-success m-0">
-        <div className="container-fluid">
-          <Link className="navbar-brand text-white" to="/">
-            MyApp
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav me-auto">
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/">
+      <Navbar
+          collapseOnSelect
+          expand="xl"
+          className='bg-success m-0'
+          expanded={expanded} // Control the expanded state
+        >
+          <Container>
+            <Navbar.Brand className="text-white" href="/">
+              MyApp
+            </Navbar.Brand>
+            <Navbar.Toggle
+              aria-controls="navbarScroll"
+              onClick={() => setExpanded(!expanded)} // Toggle the navbar state
+            />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav className="me-auto">
+                <Nav.Link
+                  className="text-white"
+                  as={Link}
+                  to="/"
+                  onClick={handleNavLinkClick}
+                >
                   Patient Search
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/pt-reg">
+                </Nav.Link>
+                <Nav.Link
+                  className="text-white"
+                  as={Link}
+                  to="/pt-reg"
+                  onClick={handleNavLinkClick}
+                >
                   Patient Registration
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/orders">
+                </Nav.Link>
+                <Nav.Link
+                  className="text-white"
+                  as={Link}
+                  to="/orders"
+                  onClick={handleNavLinkClick}
+                >
                   Orders
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/report">
+                </Nav.Link>
+                <Nav.Link
+                  className="text-white"
+                  as={Link}
+                  to="/report"
+                  onClick={handleNavLinkClick}
+                >
                   Reports
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/manage">
+                </Nav.Link>
+                <Nav.Link
+                  className="text-white"
+                  as={Link}
+                  to="/manage"
+                  onClick={handleNavLinkClick}
+                >
                   Manage
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/lab">
+                </Nav.Link>
+                <Nav.Link
+                  className="text-white"
+                  as={Link}
+                  to="/lab"
+                  onClick={handleNavLinkClick}
+                >
                   Laboratory
-                </Link>
-              </li>
-            </ul>
-            {user && (
-              <DropdownButton
-                size='sm'
-                id="dropdown-basic-button"
-                variant='success'
-                title={`${user.name}`} // Display username from token
-              >
-                <Dropdown.Item onClick={logout}>Log out</Dropdown.Item>
-              </DropdownButton>
-              
-            )}
-          </div>
-        </div>
-      </nav>
+                </Nav.Link>
+    
+              </Nav>
+              <Navbar.Text>
+                  {user && (
+                    <DropdownButton
+                      size="sm"
+                      id="dropdown-basic-button"
+                      variant="success"
+                      title={` ${user.name}`} // Display username from token
+                    >
+                      <Dropdown.Item
+                        className="w-auto"
+                        onClick={() => {
+                          logout();
+                          handleNavLinkClick(); // Collapse navbar on logout
+                        }}
+                      >
+                        Log out
+                      </Dropdown.Item>
+                    </DropdownButton>
+                  )}
+                </Navbar.Text>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
  
       <div className="px-4">
         <Outlet />
